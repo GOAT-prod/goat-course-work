8b22968aeb17da9c5ba61f296e54485974353976
i.antonov <i.antonov@smartway.today>
2025-06-15 18:33:18 +0300

    fixes

--- auth-service/api/handlers/login_handler.go
+++ auth-service/api/handlers/login_handler.go
@@ -4,6 +4,7 @@ import (
 	"auth-service/domain"
 	"auth-service/service"
 	"github.com/gofiber/fiber/v2"
+	"log"
 	"net/http"
 )
 
@@ -11,12 +12,14 @@ func LoginHandler(authService service.AuthService) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		var loginUser domain.LoginUser
 		if err := c.BodyParser(&loginUser); err != nil {
+			log.Println(err)
 			c.Status(http.StatusForbidden)
 			return err
 		}
 
 		loginResult, err := authService.Login(loginUser.Username, loginUser.Password)
 		if err != nil {
+			log.Println(err)
 			c.Status(http.StatusForbidden)
 			return err
 		}
